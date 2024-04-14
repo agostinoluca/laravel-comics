77c083ef4854d1a05ad1cc8ab581a8f67ec2d523
agostinoluca <agostino.luca89@gmail.com>
2024-04-14 13:25:12 +0200

    add new route to about page add partials of header, footer and nav

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>This is the content of the home page</h1>
+    <h1>This is the content of the about page</h1>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,20 @@
 
     <title>Laravel Comics</title>
 
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 </head>
 
 <body class="antialiased">
+    @include('partials.header')
+    @include('partials.nav')
+    <main>
+        @yield('content')
+    </main>
+    @include('partials.footer')
 </body>
 
 </html>
