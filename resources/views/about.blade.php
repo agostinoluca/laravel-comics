3c780a52b33eebe68f415b34bf2aa2a8d0dfe313
agostinoluca <agostino.luca89@gmail.com>
2024-04-14 18:39:00 +0200

    add contents to css add contents to header add jumbotron

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,5 +2,4 @@
 
 @section('content')
     <h1>This is the content of the about page</h1>
-    <img src="{{ Vite::asset('resources/images/adv.jpg') }}">
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,28 @@
 <header>
-    <h1>This is the Header</h1>
+    <div class="bg-primary">
+        <div class="container">
+            <ul class="d-flex p-1 justify-content-end text-light gap-3">
+                <li>Lorem</li>
+                <li>Ad</li>
+            </ul>
+        </div>
+    </div>
+    <div class="bg-light p-3">
+        <div class="container">
+            <ul class="d-flex gap-3 justify-content-center">
+                <li>Lorem</li>
+                <li>Ea</li>
+                <li>Voluptates</li>
+                <li>Beatae</li>
+                <li>Quo</li>
+                <li>Accusamus</li>
+                <li>Tenetur</li>
+                <li>Ipsam</li>
+                <li>Temporibus</li>
+                <li>Accusamus</li>
+            </ul>
+        </div>
+    </div>
+    <div class="jumbotron">
+    </div>
 </header>
